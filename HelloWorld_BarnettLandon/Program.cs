52db6ca8f8c4dbd4f1d27034a84bb6fcff50efd3
M1ST-hub <nmckrypt@gmail.com>
2024-02-15 10:01:10 -0600

    Initial commit

---
+++ HelloWorld_BarnettLandon/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HelloWorld_BarnettLandon
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Allows to grab first and last names
+            string firstName;
+            string lastName;
+
+            //prints the first question to the console
+            Console.WriteLine("Wuss yo name cuhh");
+            
+            //Allows user to respond to the question
+            firstName = Console.ReadLine();
+
+            //Prints response back to user
+            Console.WriteLine($"Wsg {firstName}");
+            Console.WriteLine($"Wuss yo last name, {firstName}");
+
+            //Allows user to respond to the second question
+            lastName = Console.ReadLine();
+
+            //Greets the user
+            Console.WriteLine($"Nice to meet you {firstName} {lastName}");
+        }
+    }
+}
